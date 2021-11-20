400f7f73e1f04b31fb7e160aeabe11a257739d2e
vinzmc <maurice.marvin@student.umn.ac.id>
2021-11-20 15:24:57 +0700

    finished jumping

--- Week11/Assets/Script/Drive.cs
+++ Week11/Assets/Script/Drive.cs
@@ -5,41 +5,80 @@ using UnityEngine.UI;
 
 public class Drive : MonoBehaviour
 {
+    Animator anim;
+    Rigidbody rb;
+    public Text label;
     public float speed = 5.0f;
     public float rotationSpeed = 100.0f;
-    Animator anim;
-    public Text label;
+    public float jumpheight = 1.6f;
+    
     int counter = 0;
 
-    void Awake() {
+    void Awake()
+    {
         anim = this.GetComponent<Animator>();
-    }
-    void FixedUpdate() {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-        translation *= Time.fixedDeltaTime;
-        rotation *= Time.fixedDeltaTime;
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
-
-        if(translation != 0){
-            anim.SetBool("isWalking", true);
-            anim.SetFloat("characterSpeed", translation);
-        }else{
-            anim.SetBool("isWalking", false);
-            anim.SetFloat("characterSpeed", 0);
-        }
+        rb = GetComponent<Rigidbody>();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+    void FixedUpdate()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        translation *= Time.fixedDeltaTime;
+        transform.Translate(0, 0, translation);
+
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        rotation *= Time.fixedDeltaTime;
+        transform.Rotate(0, rotation, 0);
+
+        if (translation != 0)
+        {
+            if (!anim.GetBool("isJumping"))
+            {
+                if (translation > 0 && speed < 8f)
+                {
+                    speed += 0.05f;
+                }
+                anim.SetBool("isWalking", true);
+                anim.SetFloat("characterSpeed", translation);
+            }
+        }
+        else
+        {
+            speed = 5f;
+            anim.SetBool("isWalking", false);
+            anim.SetFloat("characterSpeed", 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && !anim.GetBool("isJumping"))
+        {
+            Vector3 newVelocity = new Vector3(rb.velocity.x, jumpheight, rb.velocity.z);
+            rb.velocity = newVelocity;
+
+            anim.SetBool("isJumping", true);
+        }
+        if (anim.GetBool("isJumping"))
+        {
+            speed -= 0.001f;
+            translation *= Time.fixedDeltaTime;
+            anim.SetFloat("characterSpeed", translation);
+        }
+
+        if(anim.GetFloat("characterSpeed") > 0.12f){
+            anim.SetBool("isRunning", true);
+        }else{
+            anim.SetBool("isRunning", false);
+        }
+        Debug.Log(rb.velocity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnCollisionEnter(Collision col)
@@ -50,5 +89,6 @@ public class Drive : MonoBehaviour
             label.text = counter.ToString();
             Destroy(col.gameObject);
         }
+        anim.SetBool("isJumping", false);
     }
 }
